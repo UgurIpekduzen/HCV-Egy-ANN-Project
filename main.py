3b0a05bef0d6527c9f8de20bdef5e3429c193f9e
Göktuğ Özdemir <bgoktugozdemir@gmail.com>
2019-11-29 22:06:51 +0300

    Tested and Updated

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ import numpy
 
 dataset = numpy.loadtxt("./input/HCV-Egy-Data.csv", delimiter=",")
 
-X = dataset[:600,0:27]
-Y = dataset[:600,27:29]
+X = dataset[:600, 0:27]
+Y = dataset[:600, 27:29]
+print(X)
+print(Y)
 
 model = Sequential()
 
@@ -24,17 +26,17 @@ model.add(Dense(12, init='uniform', activation='relu'))
 # Üçüncü katmanımızda 8 yapay hücremiz var.
 model.add(Dense(8, init='uniform', activation='sigmoid'))
 
-# Dördüncü katmanımızda 1 yapay hücremiz var. Yani çıkışımız.
+# Dördüncü katmanımızda 2 yapay hücremiz var. Yani çıkışımız.
 model.add(Dense(2, init='uniform', activation='sigmoid'))
 
 # Modelimizi derliyoruz.
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X, Y, nb_epoch=150, batch_size=10,  verbose=2)
+model.fit(X, Y, nb_epoch=150, batch_size=10, verbose=2)
 
 scores = model.evaluate(X, Y)
 
-print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
 # 96 adet test verimizin sadece 27 girdisini sisteme veriyoruz.
 test_verisi = dataset[600:696, 0:27]
@@ -44,7 +46,7 @@ predictions = model.predict(test_verisi)
 
 dogru = 0
 yanlis = 0
-toplam_veri = len(dataset[600:696, 27:29])
+toplam_veri = len(dataset[600:696, 27])
 
 for x, y in zip(predictions, dataset[600:696, 27]):
     x = int(numpy.round(x[0]))
