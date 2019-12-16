25a0bf705cec50c005cc109f0f715da392f54691
Göktuğ Özdemir <bgoktugozdemir@gmail.com>
2019-12-16 22:46:59 +0300

    main updated

--- main10.py
+++ main10.py
@@ -9,18 +9,17 @@ from sklearn.utils import shuffle
 from termcolor import cprint
 from dataset import *
 
-
 dataset = setDataFrame()
-print(dataset)
 
 data = shuffle(dataset)
 
 X = dataset.drop(['Baseline_Histological_Staging'], axis=1)
 X = np.array(X)
 Y = dataset['Baseline_Histological_Staging']
+
+print("Repeating outputs are analyzing...")
 stageNumbers = findRepeatedElements(Y)
 stageNames = setStageNames(sorted(stageNumbers))
-
 
 encoder = LabelEncoder()
 encoder.fit(Y)
@@ -31,34 +30,30 @@ Y = to_categorical(Y)
 # Y2 = encoder.transform(Y2)
 # Y2 = to_categorical(Y2)
 
-trainX, testX, trainY, testY = model_selection.train_test_split(X, Y,test_size = 0.231, random_state = 0)
+trainX, testX, trainY, testY = model_selection.train_test_split(X, Y, test_size=0.231, random_state=0)
 
-model = Sequential()
+model = Sequential([
+    Dense(100, activation="relu", input_dim=28),
+    Dense(100, activation="tanh"),
+    Dense(100, activation="tanh"),
+    Dense(100, activation="tanh"),
+    Dense(4, activation="sigmoid")
+])
 
-model.add(Dense(160, activation="tanh", input_dim=28))
-# model.add(Dense(100, activation="tanh"))
-# model.add(Dense(100, activation="tanh"))
-# model.add(Dense(100, activation="tanh"))
-model.add(Dense(4, activation="sigmoid"))
+# model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.0001), metrics=[mse, categorical_accuracy])
+model.compile(loss='mse', optimizer=RMSprop(lr=0.0001), metrics=['accuracy'])
 
-model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.0001, rho=0.9), metrics=[mse,categorical_accuracy])
-model.fit(trainX, trainY, batch_size=64, verbose=1, epochs=70)
+model.fit(trainX, trainY, batch_size=64, verbose=1, epochs=1000)
 scores = model.evaluate(trainX, trainY)
 
-
-print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
 # Tahmin sonuçlarının decimal hale çevrilmesi
 binaryPredictions = model.predict(testX)
 predictions = np.argmax(binaryPredictions, axis=1) + 1
 
 targets = np.argmax(testY, axis=1) + 1
-confusionMatrix = plot_cnf_matrix(predicted=setStageNames(predictions), target=setStageNames(targets),
-                classes=stageNames
-                ,normalize=False)
 
-# plot_roc(X_train=trainX, X_test=testX, Y_train= trainY, Y_test=testY, stage_names=stageNames)
-plot_roc2(Y_test=testY, predictions=binaryPredictions, stage_names=stageNames)
 # predictionsIndexes = np.argmax(binaryPredictions, axis=1)
 # decimalPredictions = predictionsIndexes + 1
 #
@@ -80,3 +75,10 @@ for x, y in zip(predictions, targets):
 print("\n", "-" * 150, "\nISTATISTIK:\nToplam ", toplam_veri, " Veri içersinde;\nDoğru Bilme Sayısı: ", dogru,
       "\nYanlış Bilme Sayısı: ", yanlis,
       "\nBaşarı Yüzdesi: ", str(int(100 * dogru / toplam_veri)) + "%", sep="")
+
+confusionMatrix = plot_cnf_matrix(predicted=setStageNames(predictions), target=setStageNames(targets),
+                                  classes=stageNames
+                                  , normalize=False)
+
+# plot_roc(X_train=trainX, X_test=testX, Y_train= trainY, Y_test=testY, stage_names=stageNames)
+plot_roc2(Y_test=testY, predictions=binaryPredictions, stage_names=stageNames)
